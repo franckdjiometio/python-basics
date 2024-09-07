c06a86aa24a13229956555405d5d5b9d778ec791
Franck Djiometio <franck.djiometio@gmx.de>
2024-09-07 10:04:50 +0200

    Variable Assignment and String Manipulation

---
+++ app.py
@@ -0,0 +1,11 @@
+print("1. Exercises: Variable Assignment and String Manipulation")
+print("Write a program that asks the user for their name and age, then prints a greeting message using string formatting")
+
+print("--------------------------------")
+user_name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+print("Hello " + user_name + ". I hear you are " + user_age + " years old.")
+
+print("--------------------------------")
+print("2. Exercises: Basic Arithmetic and User Input")
+print("Create a simple calculator that takes two numbers as input and performs addition, subtraction, multiplication, and division.")
